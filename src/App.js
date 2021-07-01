52c2fdc1c867b0159569dfb34882513e1cf30a7a
nadhif_rafifaiz <nadhifrafifaiz@gmail.com>
2021-07-01 23:25:26 +0700

    State and Props

--- src/App.js
+++ src/App.js
@@ -4,22 +4,70 @@ import './styles.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import TodoItem from './components/TodoItem';
 import TodoItemFunction from './components/TodoItemFunction';
+import React from 'react';
 // Props
 // -Data dari parent ke child component
 // -Parent component yang membungkus child component
 
-function App() {
-  const kegiatan = "Makan";
+class App extends React.Component {
+  // State
+  // -Mengirim data ke component itu sendiri
+  state = {
+    todoList: [
+      { activity: "Makan", id: 1 },
+      { activity: "Lari", id: 2 },
+      { activity: "Pulang", id: 3 },
+    ],
+    inputTodo: ""
+  }
 
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <TodoItem todoData={{ activity: "Lari", id: 1 }} />
-      <TodoItem todoData={{ activity: "Mandi", id: 2 }} />
+  deleteTodo = (id) => {
+    this.setState({
+      todoList: this.state.todoList.filter((val) => {
+        return val.id != id
+      })
+    })
+  }
 
-      <TodoItemFunction todoData={{ activity: "Jalan", id: 2 }} />
-    </div>
-  );
+  renderTodoList = () => {
+    // return this.statement itu untuk lempar nilai ke baris 44
+    return this.state.todoList.map((val) => {
+      return (
+        <TodoItem deleteTodoHandler={this.deleteTodo} todoData={val} />
+      )
+    })
+  }
+
+  addTodo = () => {
+    this.setState({
+      todoList: [
+        ...this.state.todoList, { activity: this.state.inputTodo, id: this.state.todoList.length + 1 }
+      ]
+    })
+  }
+
+
+  inputHandler = (event) => {
+    this.setState({ inputTodo: event.target.value })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        {/* Merender barisan todo item dari fungsi renderTodoList() */}
+        {this.renderTodoList()}
+
+        <div>
+          <input onChange={this.inputHandler} type="text" className="mx-3" />
+          <button className="btn btn-primary" onClick={this.addTodo}>Add Todo</button>
+        </div>
+
+
+      </div >
+    );
+  }
 }
+
 
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -15,7 +15,7 @@ class TodoItem extends React.Component {
             <div className="my-1 d-flex flex-row justify-content-between todo-item-container align-items-center">
                 {this.props.todoData.activity} ID: {this.props.todoData.id}
                 <div>
-                    <button onClick={this.deleteButtonHandler} className="btn btn-danger">Delete</button>
+                    <button onClick={() => this.props.deleteTodoHandler(this.props.todoData.id)} className="btn btn-danger">Delete</button>
                     <button onClick={() => this.btnHandler("COMPLETE")} className="btn btn-success">Complete</button>
                 </div>
             </div>
